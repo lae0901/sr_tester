f8fd83133679a1e67ed7c67c7c2364330e925a04
lae0901 <stephenrichter@gmail.com>
2020-06-11 19:55:12 -0400

    path_getFileName

--- src/browser-path.ts
+++ src/browser-path.ts
@@ -1,10 +1,14 @@
 
 // ------------------------- path_getFileName ------------------------
+
+import { scan_revCharEqAny } from "./main";
+
 // return the part of the path that follows the last "/" in the string.
 export function path_getFileName(path : string ) : string
 {
 	let fileName = path;
-	const fx = path.lastIndexOf('/');
+	const fx = scan_revCharEqAny(path, path.length - 1, '\\/' );
+	// let fx = path.lastIndexOf('/');
 	const bx = fx + 1;
 	if ((bx > 0) && (bx < path.length))
 	{
